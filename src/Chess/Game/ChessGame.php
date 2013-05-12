a5cabd19a60523e0b4093a30db8bf3ea26e74ed5
Mark Badolato <mbadolato@cybernox.com>
2013-05-12 05:01:29 -0700

    Fix horribly broken getMoveListString() logic
    The method locked to see if withChecked was set and, if so, then
    got the moves list with the checks. The problem is, it immediately
    afterwards, regardless of withChecked status overwrote the moveList
    with the non-check version of the moves.

--- src/Chess/Game/ChessGame.php
+++ src/Chess/Game/ChessGame.php
@@ -1134,8 +1134,9 @@ class ChessGame
     {
       if ($withChecks) {
         $objMoveList = $this->_movesWithCheck;
+      } else {
+        $objMoveList = $this->_moves;
       }
-      $objMoveList = $this->_moves;
       $strMoveList = "";
       $intCount=0;
       foreach ($objMoveList as $key => $objMove) {
